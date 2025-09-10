69763278d90122dd21e11a2bf885016a16840466
Yoshida672 <ericissamuyoshida@gmail.com>
2025-09-10 09:32:26 -0300

    Criando Estrutura do projeto, ThemeContext e configurando rota Incial

---
+++ app/_layout.jsx
@@ -0,0 +1,12 @@
+
+import { Stack } from "expo-router";
+import { ThemeProvider } from "../src/context/ThemeContext";
+
+export default function Layout() {
+
+return(
+      <Stack screenOptions={{ headerShown: false }} />
+
+)
+
+}
