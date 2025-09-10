8a8da7ec08c9c0b6a08961c9c9397698ee0cb94e
Yoshida672 <ericissamuyoshida@gmail.com>
2025-09-10 11:46:08 -0300

    Implementando metodo de login com autenticação de email e senha no Firebase

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,28 @@
 import { useState } from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
-
+import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {auth} from "../src/services/firebaseConfig"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "expo-router";
 export default function Login(){
     const [email,setEmail]= useState("");
     const [senha,setSenha]= useState("");
+    const router = useRouter()
+    const handleLogin=()=>{
+    if(!email || !senha ){
+        Alert.alert("Atenção!","Preencha todos os campos!");
+    }
+    else{
+    signInWithEmailAndPassword(auth,email,senha).then(
+        async(userCredential)=>{
+            const user = userCredential.user
+            router.push("/HomeScreen")
+
+        }
+    )
+   
+}
+}
+
     return(
     <View>
      <Text>Login</Text>
@@ -18,9 +37,10 @@ export default function Login(){
      value={senha}
      onChangeText={setSenha}   
  />
-     <TouchableOpacity>
+     <TouchableOpacity onPress={handleLogin}>
         <Text>Login</Text>
      </TouchableOpacity>
+
     </View>
     );
 }
