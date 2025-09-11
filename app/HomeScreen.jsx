c48d441d48dd058d000c369875370441ab9517aa
Gustavo295 <gustavomteixeira.2006@gmail.com>
2025-09-11 11:55:56 -0300

    puxando api

--- app/HomeScreen.jsx
+++ app/HomeScreen.jsx
@@ -1,11 +1,43 @@
 import React from "react";
-import { View ,Text} from "react-native";
+import { View ,Text, FlatList} from "react-native";
+import { useQuery } from "@tanstack/react-query";
+import { fetchCharacter } from "../src/api/marvelApi";
 
 export default function HomeScreen(){
 
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["marvel-characters"],
+    queryFn: fetchCharacter,
+  });
+
+  if (isLoading) {
+    return <Text>Carregando...</Text>
+  }
+
+  if (error) {
+    return <Text>Erro: {error}</Text>
+  }
+
 return(
     <View>
-        <Text>Home</Text>
+        <Text>Personagens</Text>
+        <FlatList
+            data={data}
+            keyExtractor={(item) => item.data.results.id}
+            renderItem={({ item }) => (
+        <View>
+            <Image
+              source={{
+                uri: `${item.thumbnail.path}.${item.thumbnail.extension}`,
+              }}
+              style={{width:100,height:100}}
+            />
+
+            <Text style={{fontSize:40}}>{item.data.results.name}</Text>
+            
+          </View>
+        )}
+      />
     </View>
 );
     
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,13 +1,15 @@
-
 import { Stack } from "expo-router";
 import { ThemeProvider } from "../src/context/ThemeContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function Layout() {
-
-return(
+  return (
     <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }} />
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </QueryClientProvider>
     </ThemeProvider>
-)
-
+  );
 }
