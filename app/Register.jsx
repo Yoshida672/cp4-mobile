f9dd310ef431df9566eb6c7abdfe6f4729c4a1c9
Yoshida672 <ericissamuyoshida@gmail.com>
2025-09-14 13:02:17 -0300

    feat: autenticacao de credenciais

--- app/Register.jsx
+++ app/Register.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { auth } from "../src/services/firebaseConfig";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  validatePassword,
+} from "firebase/auth";
 import {
   Alert,
   Text,
@@ -21,24 +24,78 @@ export default function Register() {
   const [senha, setSenha] = useState("");
   const { t } = useTranslation();
 
-  const handleRegister = () => {
+  const handleRegister = async () => {
     if (!nome || !email || !senha) {
       Alert.alert(t("login.alerts.title"), t("login.alerts.message"));
       return;
     }
-    createUserWithEmailAndPassword(auth, email, senha)
-      .then(() => {
-        router.back();
-      })
-      .catch((error) => {
-        Alert.alert(t("login.alerts.error"), error.message);
-      });
+    try {
+      const status = await validatePassword(auth, senha);
+      if (!status.isValid) {
+        let msg = "Senha inválida. Requisitos: \n";
+        if (status.containsLowercaseLetter === false)
+          msg += "- Pelo menos uma letra minúscula\n";
+        if (status.containsUppercaseLetter === false)
+          msg += "- Pelo menos uma letra maiúscula\n";
+        if (status.containsNumericCharacter === false)
+          msg += "- Pelo menos um número\n";
+        if (status.containsNonAlphanumericCharacter === false)
+          msg += "- Pelo menos um caractere especial\n";
+        if (
+          status.passwordPolicy.customStrengthOptions.minPasswordLength !==
+            undefined &&
+          senha.length <
+            status.passwordPolicy.customStrengthOptions.minPasswordLength
+        )
+          msg += `- Mínimo ${status.passwordPolicy.customStrengthOptions.minPasswordLength} caracteres\n`;
+        Alert.alert("Senha inválida", msg);
+        console.log(status);
+        return;
+      }
+      await createUserWithEmailAndPassword(auth, email, senha);
+      router.back();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/weak-password":
+          Alert.alert(
+            t("register.alerts.error.title"),
+            t("register.alerts.error.weakPassword")
+          );
+          break;
+        case "auth/email-already-in-use":
+          Alert.alert(
+            t("register.alerts.error.title"),
+            t("register.alerts.error.emailInUse")
+          );
+          break;
+        case "auth/invalid-email":
+          Alert.alert(
+            t("register.alerts.error.title"),
+            t("register.alerts.error.invalidEmail")
+          );
+          break;
+        case "auth/invalid-password":
+          Alert.alert(
+            t("register.alerts.error.title"),
+            t("register.alerts.error.invalidPassword")
+          );
+          break;
+        default:
+          Alert.alert(
+            t("register.alerts.error.title"),
+            t("register.alerts.error.generic")
+          );
+          break;
+      }
+    }
   };
 
   return (
     <View style={{ ...styles.container, backgroundColor: colors.background }}>
       <ThemeToggleButton />
-      <Text style={{ ...styles.title, color: colors.text }}>{t("register.title")}</Text>
+      <Text style={{ ...styles.title, color: colors.text }}>
+        {t("register.title")}
+      </Text>
       <TextInput
         style={styles.input}
         placeholder={t("register.name")}
